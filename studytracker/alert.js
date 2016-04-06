03df60f2b91914179041d57bb6f1584009029d95
Luke Dercher <luke.dercher@gmail.com>
2016-04-05 20:00:20 -0500

    Update alert.js
    sends  more messages to background.js. To tell it when to pause the study timer and start it back up, and start the bad timer if neccessary.

--- studytracker/alert.js
+++ studytracker/alert.js
@@ -1,13 +1,18 @@
 
-window.onload = function() {
-	 
-		if (confirm("Are you sure you want get on social media? (Click OK to pause the study timer. Click Cancel to redirect to Google") == true) {
-  var port = chrome.runtime.connect({name: "message"});
-port.postMessage({message: "pause"});
+window.onload = function() 
+{
+	 var port = chrome.runtime.connect({name: "message"});
+	port.postMessage({message: "pause"});
+
+	if (confirm("Are you sure you want get on social media? (Click Cancel to redirect to Google") == true) 
+	{
+		port.postMessage({message: "startbad"});
     } 
-    else {
+    else 
+    {
         location = 'http://www.google.com';
+        port.postMessage({message: "start"});
        
     }
 
-	}
+}
