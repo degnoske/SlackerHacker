f174c586430b21f76db417fcc799e9c3e5144883
Luke Dercher <luke.dercher@gmail.com>
2016-04-05 20:10:06 -0500

    Update background.js
    added a method to pause the bad timer when the window comes off of focus of facebook and starts the good timer.

--- studytracker/background.js
+++ studytracker/background.js
@@ -132,3 +132,23 @@ chrome.runtime.onConnect.addListener(function(port)
 	  }
   });
 });
+
+//taken from this example http://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
+//checks if current tab is not facebook. If it isn't it starts the timer and pauses the bad timer. 
+//not verifiably working yet. No way to see bad timer.
+
+chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+
+     // since only one tab should be active and in the current window at once
+     // the return variable should only have one entry
+     var activeTab = arrayOfTabs[0];
+     var activeTabURL = activeTab.url;; // or do whatever you need
+	
+	if(activeTabURL != "https://www.facebook.com/")
+	{	
+		BadPause();
+		
+		Start();
+
+	}
+  });
