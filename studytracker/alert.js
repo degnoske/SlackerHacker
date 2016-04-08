094b2941dc9cfd6551efd503814ca6fccfe47d63
Luke Dercher <luke.dercher@gmail.com>
2016-04-08 09:51:03 -0500

    Update alert.js
    added documentation and authors.

--- studytracker/alert.js
+++ studytracker/alert.js
@@ -1,4 +1,6 @@
-
+/* author:Luke Dercher, Dylan Egnoske
+Creates a prompt that will direct pause the good timer and start the bad timer or restart goot timer ad redirect the user to google.com*/
+  
 window.onfocus = function() 
 {
 	 var port = chrome.runtime.connect({name: "message"});
