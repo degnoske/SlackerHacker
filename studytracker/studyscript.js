1ba6ba18d1b2e4f3757b847c92fee1c568555b96
degnoske <degnoske@gmail.com>
2016-04-08 16:24:51 -0500

    comments for documentation

--- studytracker/studyscript.js
+++ studytracker/studyscript.js
@@ -1,65 +1,69 @@
-/* @author: Dylan Egnoske, Luke Dercher
-   This js file runs the functions in background.js when the appropirate buttons are pressed. */
- var bg = chrome.extension.getBackgroundPage();
- 
- 
+/**
+ *  @author: Dylan Egnoske, Luke Dercher
+ *This js file runs the functions in background.js when the appropirate buttons are pressed.
+ */
+var bg = chrome.extension.getBackgroundPage();
+
+
 window.onload = function() {
-	
-	
-   
+
+
+
     var start = document.getElementById("start");
     start.onclick = start_time;
 
-    
+
     var pause = document.getElementById("pause");
     pause.onclick=pause_time;
 
-   
+
 
     var stop = document.getElementById("stop");
     stop.onclick=stop_time;
-	
 
-	if(bg.get_isStarted()==true){
-	document.getElementById("start").value = "Studying";
-	}
-	else{
-	document.getElementById("start").innerHTML = "Start Studying";
-	}
+
+    if(bg.get_isStarted()==true){
+        document.getElementById("start").value = "Studying";
+    }
+    else{
+        document.getElementById("start").innerHTML = "Start Studying";
+    }
 };
 
-/*   /**
-   * updates time Display every 1000 ms
-   *
-  function view_time() {
-         setInterval(function () {
-          document.getElementById("time").innerHTML = formatTime();
-         }, 1000);
-
-  } */
+/**
+ * @author: Dylan Egnoske
+ * @returns: null
+ */
 function pause_time(){
     bg.Pause();
 
 }
-  function start_time() {
+/**
+ * @author: Dylan Egnoske
+ * @returns: null
+ */
+function start_time() {
 
-      bg.Start();
-      document.getElementById("start").value = "Studying";
-  }
+    bg.Start();
+    document.getElementById("start").value = "Studying";
+}
 
-
+/**
+ * @author: Dylan Egnoske
+ * @returns: null
+ */
 
 function stop_time() {
 
     bg.Stop();
     document.getElementById("start").innerHTML = "Start Studying!";
 }
-  /**
-   *
-   * @returns Formatted Time String
-   */
-  function formatTime(){
-   var totalsec= bg.get_sec();
+/**
+ *
+ * @returns Formatted Time String
+ */
+function formatTime(){
+    var totalsec= bg.get_sec();
 
     var hr  = Math.floor(totalsec / 3600);
     var min = Math.floor((totalsec - (hr * 3600))/60);
@@ -72,6 +76,7 @@ function stop_time() {
     return("Study Time: " + hr + ":" + min + ":" + sec)
 
 }
+
 
 
 //taken from this example http://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
