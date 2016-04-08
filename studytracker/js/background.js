4df695b26a5ff4e65a3f02fa3e91d554325df5da
srigayatrisundar <sri@ku.edu>
2016-04-08 09:12:45 -0500

    added comments
    Added comments

--- studytracker/js/background.js
+++ studytracker/js/background.js
@@ -1,7 +1,6 @@
+//Author: SRI GAYATRI
 chrome.webNavigation.onCompleted.addListener(function(e)
 	{
-		
-		//alert("html page loads");
 		var activeTab = window.location.hostname; 
 		var url =e.url;
 		var n1 = url.indexOf("www.");
@@ -17,6 +16,7 @@ chrome.webNavigation.onCompleted.addListener(function(e)
 					$('#foo').html(data);
 					if(document.getElementById("foo").innerHTML == 1)
 					{
+						//Author Luke Dercher
 						var port = chrome.runtime.connect({name: "message"});
 						port.postMessage({message: "pause"});
 
@@ -38,6 +38,7 @@ chrome.webNavigation.onCompleted.addListener(function(e)
 		
 		
 	});
+//Author: Dylan Egnoske
 var sec=0;
 var myTime;
 var isStarted=false;
@@ -91,6 +92,7 @@ function get_sec(){
 
 }
 
+//Author: Luke Dercher
 ///////////////////
 ////Bad Timer/////
 //////////////////
@@ -172,22 +174,4 @@ chrome.runtime.onConnect.addListener(function(port)
   });
 });
 
-//taken from this example http://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
-//checks if current tab is not facebook. If it isn't it starts the timer and pauses the bad timer. 
-//not verifiably working yet. No way to see bad timer.
 
-/*chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-
-     // since only one tab should be active and in the current window at once
-     // the return variable should only have one entry
-     var activeTab = arrayOfTabs[0];
-     var activeTabURL = activeTab.url;; // or do whatever you need
-	
-	if(activeTabURL != "https://www.facebook.com/")
-	{	
-		BadPause();
-		
-		Start();
-
-	}
-  });*/
