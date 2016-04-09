26d888e3b05bdf8f64fe5b947d9651f2df87dcdb
Vuong Nguyen <nptvuong2912@gmail.com>
2016-04-08 22:01:30 -0500

    connect to firebase and initialize the array

--- studytracker/js/todo.js
+++ studytracker/js/todo.js
@@ -3,4 +3,10 @@ var todoApp = angular.module('TodoApp', ['firebase']);
 todoApp.controller('TodoCtrl', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray) {
         
+	// CREATE A FIREBASE REFERENCE
+    var todosRef = new Firebase('your-firebase-app.firebase.io');
+
+    // GET TODOS AS AN ARRAY
+    $scope.todos = $firebaseArray(todosRef);
+	
 }]);
