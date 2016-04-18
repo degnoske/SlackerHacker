9a7165aa123c773c66dfe0ac085ed4c60d9bbd94
Luke Dercher <luke.dercher@gmail.com>
2016-04-18 11:57:12 -0500

    Update studyscript.js
    used bg to make a new good Timer obejct in sudyscript. Fixed bug

--- studytracker/studyscript.js
+++ studytracker/studyscript.js
@@ -1,53 +1,12 @@
 
  var bg = chrome.extension.getBackgroundPage();
-  var port = chrome.runtime.connect({name: "exTimers"});
 
-
-
-  /**
-   * updates time Display every 1000 ms
-   */
-  //function view_time() {
-    //     setInterval(function () {
-      //    document.getElementById("time").innerHTML = formatTime();
-      //   }, 1000);
-var flag = 0;
-  //}
-function pause_time(){
-    port.postMessage({timer: "Pause"});
-}
-  function start_time() {
-
-      port.postMessage({timer: "Start"});
-      flag = 1;
-  }
-
-function stop_time() {
-
-    port.postMessage({timer: "Stop"});
-    flag = 2;
-}
-  /**
-   *
-   * @returns Formatted Time String
-   */
-  function formatTime(){
-   var totalsec= bg.goodTimer.get_sec();
-
-    var hr  = Math.floor(totalsec / 3600);
-    var min = Math.floor((totalsec - (hr * 3600))/60);
-    var sec = Math.floor(totalsec - (hr * 3600) -  (min * 60));
-
-    if (hr < 10)   { hr    = "0" + hr; }
-    if (min < 10) { min = "0" + min; }
-    if (sec < 10)  { sec  = "0" + sec; }
-
-    return("Study Time: " + hr + ":" + min + ":" + sec)
-
-}
-
+var goodTimer = new bg.Timer();
 
 window.onload = function() {
+
+
+
     var start = document.getElementById("start");
     start.onclick = start_time;
 
@@ -64,22 +23,68 @@ window.onload = function() {
     var stop = document.getElementById("stop");
     stop.onclick=stop_time;
 
-    if(flag == 1)
-    {
-      document.getElementById("start").value = "Studying";
-    }
-    else if (flag == 2)
-    {
-      document.getElementById("start").value = "Start Studying!";
-    }
-  }
-
 	//var viewStats = document.getElementById("viewStats");
 	//viewStats.onclick=Hello;
 
+	if(goodTimer.get_isStarted()==true){
+	document.getElementById("start").value = "Studying";
+	}
+	else{
+	document.getElementById("start").value = "Start Studying";
+	}
 
 	//var viewTime = document.getElementById("time");
 	//viewTime = view_time();
+};
+
+  /**
+   * updates time Display every 1000 ms
+   */
+  //function view_time() {
+    //     setInterval(function () {
+      //    document.getElementById("time").innerHTML = formatTime();
+      //   }, 1000);
+
+  //}
+function pause_time(){
+    goodTimer.Pause();
+    console.log("paused good timer");
+    document.getElementById("start").value = "Start Studying!";
+
+}
+  function start_time() {
+
+      goodTimer.Start();
+      document.getElementById("start").value = "Studying";
+      console.log("started good timer");
+  }
+
+
+
+function stop_time() {
+
+    goodTimer.Stop();
+    document.getElementById("start").value = "Start Studying!";
+    console.log("stopped good timer");
+}
+  /**
+   *
+   * @returns Formatted Time String
+   */
+  function formatTime(){
+   var totalsec= goodTimer.get_sec();
+
+    var hr  = Math.floor(totalsec / 3600);
+    var min = Math.floor((totalsec - (hr * 3600))/60);
+    var sec = Math.floor(totalsec - (hr * 3600) -  (min * 60));
+
+    if (hr < 10)   { hr    = "0" + hr; }
+    if (min < 10) { min = "0" + min; }
+    if (sec < 10)  { sec  = "0" + sec; }
+
+    return("Study Time: " + hr + ":" + min + ":" + sec)
+
+}
 
 
 //taken from this example http://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
