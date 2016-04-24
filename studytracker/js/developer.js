a71b396675dcc483c1c3f33b819889f949edfa45
Luke Dercher <luke.dercher@gmail.com>
2016-04-24 16:18:14 -0500

    Update developer.js
    dded more tests to display when timer is stopped, paused, started

--- studytracker/js/developer.js
+++ studytracker/js/developer.js
@@ -7,6 +7,33 @@ function changeDevDisp(){
 	{
 		document.getElementById('startPage').innerHTML = "timer started";
 	}
+  else
+  {
+      document.getElementById('startPage').innerHTML = "start timer";
+  }
+
+  if(bg.goodTimer.get_isPaused())
+	{
+		document.getElementById('pausePage').innerHTML = "timer paused";
+	}
+  else
+  {
+      document.getElementById('pausePage').innerHTML = "pause timer";
+  }
+
+  if(bg.goodTimer.get_isStarted()==true)
+	{
+		document.getElementById('stopPage').innerHTML = "stop timer";
+	}
+  else
+  {
+      if(!bg.goodTimer.get_isPaused())
+      {
+      document.getElementById('stopPage').innerHTML = "timer stopped";
+      }
+  }
+
+
 }
 
 setInterval(changeDevDisp,1000);
