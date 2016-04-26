dae30dbe5ab1d11cb9292855e95934f4f2606794
LukeWeber12 <luke12weber@yahoo.com>
2016-04-26 10:48:19 -0500

    Update storage.js to be local storage

--- studytracker/js/storage.js
+++ studytracker/js/storage.js
@@ -9,34 +9,21 @@ function StorageObj(aKey)
 {
     this.mKey = aKey;
 	//getValue doesn't work at the moment
-/* 
+
     this.getValue = function()
     {
-		//@callback doesn't work
-      chrome.storage.local.get(this.mKey, function(obj)
-      {
 
-          if(!chrome.runtime.lastError)
-          {
+      return(localStorage.getItem(this.mKey))
+    }
 
-           
-          }
-      })
-    } */
-	
 	/**
      * @author Luke Weber
      * @pre the value to send to storage.
      */
     this.setValue = function(aVal)
     {
-      var obj = {};
-      obj[aKey] = aVal;
-     chrome.storage.local.set(obj, function()
-      {//alert("saved")
-        });
+
+     localStorage.setItem(this.mKey, aVal)
+
     }
 }
-
-
-
