6592643153af70a680e97574747e02461dd303ef
Vuong Nguyen <nptvuong2912@gmail.com>
2016-04-27 00:03:01 -0500

    add todoApp div

--- studytracker/js/todo.js
+++ studytracker/js/todo.js
@@ -3,10 +3,39 @@ var todoApp = angular.module('TodoApp', ['firebase']);
 todoApp.controller('TodoCtrl', ['$scope', '$firebaseArray',
     function($scope, $firebaseArray) {
         
-	// CREATE A FIREBASE REFERENCE
-    var todosRef = new Firebase('your-firebase-app.firebase.io');
+    var todosRef = new Firebase('https://glaring-heat-8318.firebaseio.com/');
 
-    // GET TODOS AS AN ARRAY
     $scope.todos = $firebaseArray(todosRef);
-	
+
+    // Add TODO item
+    $scope.addTodo = function () {
+        var timestamp = new Date().valueOf();
+
+        $scope.todos.$add({id: timestamp, name: $scope.todoName, status: 'pending'});
+
+        $scope.todoName = "";
+    };
+
+    // remove a TODO
+    $scope.removeTodo = function (index, todo) {
+        if (todo.id === undefined)return;
+
+        $scope.todos.$remove(todo);
+    };
+
+    // update in progress status
+    $scope.startTodo = function (index, todo) {
+        if (todo.id === undefined)return;
+
+        todo.status = 'in progress';
+        $scope.todos.$save(todo);
+    };
+
+    // update complete status
+    $scope.completeTodo = function (index, todo) {
+        if (todo.id === undefined)return;
+
+        todo.status = 'complete';
+        $scope.todos.$save(todo);
+    };
 }]);
